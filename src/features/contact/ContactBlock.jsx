1e4389dd61be2cc81300d49c884328e04968e683
Ridwan-Lawal <lawal6382@gmail.com>
2024-06-07 16:06:09 +0100

    fixed bugs

--- src/features/contact/ContactBlock.jsx
+++ src/features/contact/ContactBlock.jsx
@@ -3,8 +3,8 @@ import ContactForm from "./ContactForm";
 function ContactBlock() {
   return (
     <div
-      id='contact'
-      className="mt-32  flex flex-col md:flex-row  gap-8 md:gap-12  md:max-w-[1150px] mx-auto "
+      id="contact"
+      className="mt-16 md:mt-32  flex flex-col md:flex-row  gap-8 md:gap-12  md:max-w-[1150px] mx-auto "
     >
       <div className="space-y-2 mt-24 md:space-y-4">
         <h2 className="text-white text-[29px] font-semibold md:text-[49px] leading-[50px]">
